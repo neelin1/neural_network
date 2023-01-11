cb36364198b8b99ffd69044b463618c8f9f4329e
neelin1 <zaneneelinprojects@gmail.com>
2023-01-10 21:00:47 -0800

    - worked on drawing system - still need to iron out

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-const canvas = document.getElementById('drawing-board')
+const canvas = document.getElementById('drawing-board'); //getting canvas document
+const toolbar = document.getElementById('toolbar');
+const ctx = canvas.getContext('2d');
+
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
+
+canvas.width = window.innerWidth - canvasOffsetX; //determines size of canvas
+canvas.height = window.innerHeight - canvasOffsetY;
+
+let isPainting = false; //drawing or not
+let lineWidth = 1; //linewidth
+
+let startX;
+let startY;
+
+/* clears canvas if clear button is clicked */
+toolbar.addEventListener('click', e => {
+  if (e.target.id === 'clear') {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+});
+
+// toolbar.addEventListener('change', e => {
+//   if (e.target.id === 'stroke') {
+//     ctx.strokeStyle = e.target.value; //if stroke color is changed, modify stroke as such
+//   }
+
+//   if (e.target.id === 'lineWidth') { //if line width is changed, modify linewidth as such
+//     lineWidth = e.target.value;
+//   }
+
+// });
+
+const draw = (e) => {
+  if (!isPainting) {
+    return;
+  }
+
+  ctx.lineWidth = lineWidth;
+  ctx.lineCap = 'round';
+
+  ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
+  ctx.stroke();
+}
+
+canvas.addEventListener('mousedown', (e) => {
+  isPainting = true;
+  startX = e.clientX;
+  startY = e.clientY;
+});
+
+canvas.addEventListener('mouseup', e => { //listen for when no longer drawing
+  isPainting = false;
+  ctx.stroke();
+  ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
